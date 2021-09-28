bf3898996f8bdd2887eb50f08ada419b67416cc0
Denis <denisr@gmail.com>
2021-09-28 15:24:22 +0200

    update grid,include jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
-document.querySelector("a").addEventListener("click",function(){
-    var clicked_text = document.querySelector(this).text()
-    var clicked_url = document.querySelector(this).attr("href")
+$("a").on("click",function(){
+    var clicked_text = $(this).text();
+    var clicked_url = $(this).attr("href");
     analytics.track("User click", {
         clicked_text: clicked_text,
         clicked_url: clicked_url
